e7138736b96e014cfebd0fd95af46d4c88d7bbab
krystian <cimafrancakrystian@gmail.com>
2018-03-29 18:03:37 +0800

    Test commit

---
+++ TestOOP/src/com/krystian/Bank.java
@@ -0,0 +1,52 @@
+package com.krystian;
+
+public class Bank {
+
+	private int accountNumber;
+	private int balance;
+	private String customerName;
+	private String email;
+	private int phoneNumber;
+	
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
